b1179898f22cb33e8d175eda3249824f5467cf22
Savan Patel <savanpateljob@gmail.com>
2020-03-16 20:52:41 +0530

    Update Favicon

--- src/component/header.js
+++ src/component/header.js
@@ -1,7 +1,17 @@
 import React from "react";
+import Box from "@material-ui/core/Box";
 
-const Header = props =>{
+const Header = props => {
+    return (
+        <Box display="flex" alignItems="center">
+            <Box flexGrow={1}>
 
+            </Box>
+            <Box flexShrink={0}>
+
+            </Box>
+        </Box>
+    );
 }
 
 export default Header;
