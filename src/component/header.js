ee5bfd09558ccb47ed9da4cb8626c7746daad274
savanpateljob@gmail.com <savanpateljob@gmail.com>
2020-03-16 22:11:10 +0530

    List add in header

--- src/component/header.js
+++ src/component/header.js
@@ -5,10 +5,12 @@ const Header = props => {
     return (
         <Box display="flex" alignItems="center">
             <Box flexGrow={1}>
-
+        
             </Box>
             <Box flexShrink={0}>
-
+                <ul>
+                    <li>home</li>
+                </ul>
             </Box>
         </Box>
     );
